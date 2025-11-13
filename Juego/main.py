5f2a651d4952631fe618f283a9805240a4331c13
Alumys <lautaro.kennedy@gmail.com>
2025-11-13 07:56:26 -0300

    PROYECTO BASE INICIADA

--- Juego/main.py
+++ Juego/main.py
@@ -1,1 +1,20 @@
-print('Hola, soy el jueguito :D')
+import pygame
+
+from assets.modules.ventana import ANCHO_PANTALLA_P as ANCHO
+from assets.modules.ventana import LARGO_PANTALLA_P as LARGO
+
+pygame.init()
+
+ventana_principal = pygame.display.set_mode((ANCHO, LARGO))
+pygame.display.set_caption("Tux's REVOLUTION")
+
+corriendo = True # bandera de bucle principal
+while corriendo:
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            corriendo = False
+        
+        pygame.display.update() # Actualizador de pantalla
+
+pygame.quit()
