2776b064608828e0f3f7c4e0c9298a2c94a55df2
Alumys <lautaro.kennedy@gmail.com>
2025-11-13 20:14:36 -0300

    Agrego icono al juego para probar

--- Juego/modules/ventana.py
+++ Juego/modules/ventana.py
@@ -1,8 +1,10 @@
 import pygame
 # CONSTANTES # 
 
+pygame.init()
 # display/pantalla principal#
 ANCHO_PANTALLA_P = 800
 LARGO_PANTALLA_P = 600
 
 # Icono #
+ICONO = pygame.image.load('assets/images/icono_temprano.png')
