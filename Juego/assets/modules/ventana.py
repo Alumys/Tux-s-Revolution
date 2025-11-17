319ea2a9bb4c036b2877ab017326699442a52a7c
Araceli1806 <yaninaaraceliquinonezgarcia@gmail.comgit config --global user.name Araceli1806>
2025-11-17 16:33:29 -0300

    actualizacion rama

---
+++ Juego/assets/modules/ventana.py
@@ -0,0 +1,8 @@
+import pygame
+# CONSTANTES # 
+
+# display/pantalla principal#
+ANCHO_PANTALLA_P = 800
+LARGO_PANTALLA_P = 600
+
+# Icono #
--- Juego/main.py
+++ Juego/main.py
@@ -1,1 +1,22 @@
-print('Hola, soy el jueguito :D')
+import pygame
+
+from modules.ventana import ANCHO_PANTALLA_P as ANCHO
+from modules.ventana import LARGO_PANTALLA_P as LARGO
+from modules.ventana import ICONO
+
+pygame.init()
+
+ventana_principal = pygame.display.set_mode((ANCHO, LARGO))
+pygame.display.set_caption("Tux's REVOLUTION")
+pygame.display.set_icon(ICONO)
+
+corriendo = True # bandera de bucle principal
+while corriendo:
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            corriendo = False
+        
+        pygame.display.update() # Actualizador de pantalla
+
+pygame.quit()
