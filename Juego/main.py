4b3e8c3b40375badb4af26ceec727e14d53c9276
Alumys <lautaro.kennedy@gmail.com>
2025-11-19 13:13:51 -0300

    Reorganizacion de la raiz main y copias de rama_lauta
    Se agrego el .gitignore para impedir que se suban archivos basura

--- Juego/main.py
+++ Juego/main.py
@@ -1,12 +1,14 @@
 import pygame
 
-from assets.modules.ventana import ANCHO_PANTALLA_P as ANCHO
-from assets.modules.ventana import LARGO_PANTALLA_P as LARGO
+from modules.ventana import ANCHO_PANTALLA_P as ANCHO
+from modules.ventana import LARGO_PANTALLA_P as LARGO
+from modules.ventana import ICONO
 
 pygame.init()
 
 ventana_principal = pygame.display.set_mode((ANCHO, LARGO))
 pygame.display.set_caption("Tux's REVOLUTION")
+pygame.display.set_icon(ICONO)
 
 corriendo = True # bandera de bucle principal
 while corriendo:
